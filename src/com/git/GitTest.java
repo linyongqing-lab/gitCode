4f40c898ea3c15da5c8309b68f12778c68e6e246
linyongqing-lab <84626092+linyongqing-lab@users.noreply.github.com>
2023-01-08 17:08:36 +0800

    Update GitTest.java

--- src/com/git/GitTest.java
+++ src/com/git/GitTest.java
@@ -9,6 +9,7 @@ public class GitTest {
         System.out.println("master");
         System.out.println("hot test");
         System.out.println("GitTest!");
+         System.out.println("!!!");
     }
 
 
